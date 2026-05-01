40cb755bdbfcc71ca1902d444dbd48a66222d5b4
Bab's <153094205+ibrahimkhalilbabikir@users.noreply.github.com>
2026-05-01 14:45:16 +0200

    ajout de script de deploiment dans le ci/cd et ouverture du port ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -224,10 +224,10 @@ data "aws_ami" "ubuntu" {
 
 # L'instance EC2
 resource "aws_instance" "mini_chat_ec2" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.small"
-  key_name     = "mini-chat-key"
-  subnet_id    = aws_subnet.mini_chat_public_subnet.id
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.small"
+  key_name               = "mini-chat-key"
+  subnet_id              = aws_subnet.mini_chat_public_subnet.id
   vpc_security_group_ids = [aws_security_group.mini_chat_sg.id]
 
   user_data = <<-EOF
